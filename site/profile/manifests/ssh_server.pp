9b6adefeae15725bdbad454c605819b274eb6358
terjean <79154007+terjean@users.noreply.github.com>
2022-11-01 13:37:01 +0100

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
     ensure => present,
     user   => 'root',
     type   => 'ssh-rsa',
-    key    => '',
+    key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQD1OJ3bej6wU06EZxScTMBKhK9fvfEHAEVZN9LTl9UWbdua5bm8pQSZMbNq+SwjYBuWVqkGBYcKQbaVrnKvK9se9xhNc8jxnfIOF0D5ArbxsYJ/t3tWodvIhR+oxeANFDVhkoGQXFF07mE4uuuoe3P+qVQfmFt/cZ4IDbLmreZuDe/5uDf7PwbaRrIBrrJ3fylCyKVN4nxhTtHcvcMo3pUa4CR6exDgxB2OeUzDZH3Zioq0PwoSKkBfLhBgfMnbUTX/exG+X+G9wIOOGYxOuHxNPK8C4TPHGVpC4XbFrfTuXo4U+ITv3fQLEDqOfM+BPJsUhpMje73YMM9lp2MJpJ6p',
   }  
 }
