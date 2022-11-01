164cfe7a04313592473807023add2a6f7125550f
terjean <79154007+terjean@users.noreply.github.com>
2022-11-01 13:02:14 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -6,7 +6,11 @@ node default {
   }
 }
 node 'master.puppet.vm' { 
-  include role::master_server
+  include role::master_server  
+    file { '/root/README':
+      ensure => file,
+      content => $fqdn,
+    }
 }
 
 node /^web/ {
