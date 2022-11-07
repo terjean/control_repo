c7a7d1d3742f12a9ccdccc8a8ee2d910b9f85bfb
terjean <79154007+terjean@users.noreply.github.com>
2022-11-07 09:44:32 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -13,7 +13,7 @@ node 'master.puppet.vm' {
     }
 }
 node 'minetest.puppet.vm' { 
-   include role:minecraft_server
+   include role::minecraft_server
 }
 node /^web/ {
    include role::app_server
