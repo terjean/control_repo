a44fbdd80e3894aba536811bf3099cf09bbed732
terjean <79154007+terjean@users.noreply.github.com>
2022-11-09 14:52:56 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -26,6 +26,6 @@ class minecraft (
   service { 'minecraft':
     ensure  => running,
     enable  => true,
-    require => [Package['java'],file["${install_dir}/eula.txt"],file['/etc/systemd/system/minecraft.service']],
+    require => [Package['java'],File["${install_dir}/eula.txt"],File['/etc/systemd/system/minecraft.service']],
   }
 }
