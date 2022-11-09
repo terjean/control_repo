8fc72425051f36b9c55ea73e6757229aad4ae6e9
terjean <79154007+terjean@users.noreply.github.com>
2022-11-09 14:50:12 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -21,7 +21,7 @@ class minecraft (
     ensure  => file,
     content => epp('minecraft/minecraft.service', {
       install_dir => $install_dir, 
-    })
+    }),
   }  
   service { 'minecraft':
     ensure  => running,
